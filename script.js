382fd5785bf0ed33989b81ad3d28ed634ca8008b
d8ta <danieltaggeselle@gmx.de>
2013-11-23 10:01:06 +0100

    new nav

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+        
+        
+/* bye mouseover the navbuttons get highlighted with a yellow border */
+        
+    $('').mouseover(function() {
+        $(this).addClass('highlighted');
+                });
+
+        $('').mouseout(function() {
+                $(this).removeClass('highlighted');
+                });
+                
+});
