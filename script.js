5c661cec77a12d4108fbd92587a7e5500afc8bcc
d8ta <danieltaggeselle@gmx.de>
2013-11-23 11:41:00 +0100

    scrolling in mobilansicht fast fertig

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-$(document).ready(function(){
-        
-        
-/* bye mouseover the navbuttons get highlighted with a yellow border */
-        
-    $('').mouseover(function() {
-        $(this).addClass('highlighted');
-                });
+$(document).ready(function() {
 
-        $('').mouseout(function() {
-                $(this).removeClass('highlighted');
-                });
-                
-});
+	// scrolling from to the intro location
+	$("#first, #firstScrolle").click(function() {
+		$('html, body').animate({
+			scrollTop : $("#intro").offset().top
+		}, 2000);
+	});
+
+	$("#second, #secondScrolle").click(function() {
+		$('html, body').animate({
+			scrollTop : $("#alpin").offset().top
+		}, 2000);
+	});
+
+	$("#third, #thirdScrolle").click(function() {
+		$('html, body').animate({
+			scrollTop : $("#board").offset().top
+		}, 2000);
+	});
+
+	$("#forth, #forthScrolle").click(function() {
+		$('html, body').animate({
+			scrollTop : $("#aktuelles").offset().top
+		}, 2000);
+
+		$("#fifth, #fifthScrolle").click(function() {
+			$('html, body').animate({
+				scrollTop : $("#termine").offset().top
+			}, 2000);
+		});
+	});
+	
+	$("#sixth, #sixthScrolle").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#foerderverein").offset().top
+    }, 2000);
+});  
+
+}); 
