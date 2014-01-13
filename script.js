e8555247c125cd9c8af540f728a3567d06ac2724
d8ta <danieltaggeselle@gmx.de>
2014-01-13 18:47:35 +0100

    reparatur der dropmenues

--- script.js
+++ script.js
@@ -41,12 +41,118 @@ $(document).ready(function() {
 
   
       var par = $('p');
-      $(".listheadone").hide();
+      $("#a1").hide();
 
-      $('.subheadone').click(function(e) {
-      $(".listheadone").slideToggle('slow');
+      $('#a').click(function(e) {
+      $("#a1").slideToggle('slow');
           e.preventDefault();
       });
+            var par = $('p');
+      $("#b1").hide();
+
+      $('#b').click(function(e) {
+      $("#b1").slideToggle('slow');
+          e.preventDefault();
+      });
+   
+      var par = $('p');
+      $("#c1").hide();
+
+      $('#c').click(function(e) {
+      $("#c1").slideToggle('slow');
+          e.preventDefault();
+      });
+   
+      var par = $('p');
+      $("#d1").hide();
+
+      $('#d').click(function(e) {
+      $("#d1").slideToggle('slow');
+          e.preventDefault();
+      });
+      
+      var par = $('p');
+      $("#e1").hide();
+
+      $('#e').click(function(e) {
+      $("#e1").slideToggle('slow');
+          e.preventDefault();
+      });
+      
+      var par = $('p');
+      $("#f1").hide();
+      $("#f2").hide();
+
+      $('#f').click(function(e) {
+      $("#f1").slideToggle('slow');
+          e.preventDefault();
+                $("#f2").slideToggle('slow');
+          e.preventDefault();
+      });  
+      
+      var par = $('p');
+      $("#g1").hide();
+
+      $('#g').click(function(e) {
+      $("#g1").slideToggle('slow');
+          e.preventDefault();
+      });          
+      
+      var par = $('p');
+      $("#h1").hide();
+      $("#h2").hide();
+
+      $('#h').click(function(e) {
+      $("#h1").slideToggle('slow');
+          e.preventDefault();
+                $("#h2").slideToggle('slow');
+          e.preventDefault();
+      });  
+      
+      var par = $('p');
+      $("#i1").hide();
+
+      $('#i').click(function(e) {
+      $("#i1").slideToggle('slow');
+          e.preventDefault();
+      });
+      
+      var par = $('p');
+      $("#j1").hide();
+
+      $('#j').click(function(e) {
+      $("#j1").slideToggle('slow');
+          e.preventDefault();
+      }); 
+      
+      var par = $('p');
+      $("#k1").hide();
+
+      $('#k').click(function(e) {
+      $("#k1").slideToggle('slow');
+          e.preventDefault();
+      });
+      
+      var par = $('p');
+      $("#l1").hide();
+
+      $('#l').click(function(e) {
+      $("#l1").slideToggle('slow');
+          e.preventDefault();
+      });      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
  
       var par = $('p');
       $(".listheadtwo").hide();
