1d67449aba73efe69be8f302d68b83986719dc81
d8ta <danieltaggeselle@gmx.de>
2014-01-18 08:25:24 +0100

    update: links verändert (kein unterlink mehr & SatzungsplatzhalterPDF entfernt

--- script.js
+++ script.js
@@ -38,47 +38,6 @@ $(document).ready(function() {
 	});
 
 	// togglefunction for the subnavigation
-
-
-      var par = $('p');
-      $("#a1").hide();
-
-      $('#a').click(function(e) {
-      $("#a1").slideToggle('slow');
-          e.preventDefault();
-      });
-            var par = $('p');
-      $("#b1").hide();
-
-      $('#b').click(function(e) {
-      $("#b1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#c1").hide();
-
-      $('#c').click(function(e) {
-      $("#c1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#d1").hide();
-
-      $('#d').click(function(e) {
-      $("#d1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#e1").hide();
-
-      $('#e').click(function(e) {
-      $("#e1").slideToggle('slow');
-          e.preventDefault();
-      });
-
       var par = $('p');
       $("#f1").hide();
       $("#f2").hide();
@@ -87,14 +46,6 @@ $(document).ready(function() {
       $("#f1").slideToggle('slow');
           e.preventDefault();
                 $("#f2").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#g1").hide();
-
-      $('#g').click(function(e) {
-      $("#g1").slideToggle('slow');
           e.preventDefault();
       });
 
@@ -108,75 +59,4 @@ $(document).ready(function() {
                 $("#h2").slideToggle('slow');
           e.preventDefault();
       });
-
-      var par = $('p');
-      $("#i1").hide();
-
-      $('#i').click(function(e) {
-      $("#i1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#j1").hide();
-
-      $('#j').click(function(e) {
-      $("#j1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#k1").hide();
-
-      $('#k').click(function(e) {
-      $("#k1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $("#l1").hide();
-
-      $('#l').click(function(e) {
-      $("#l1").slideToggle('slow');
-          e.preventDefault();
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      var par = $('p');
-      $(".listheadtwo").hide();
-
-      $('.subheadtwo').click(function(e) {
-      $(".listheadtwo").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $(".listheadthree").hide();
-
-      $('.subheadthree').click(function(e) {
-      $(".listheadthree").slideToggle('slow');
-          e.preventDefault();
-      });
-
-      var par = $('p');
-      $(".listheadfour").hide();
-
-      $('.subheadfour').click(function(e) {
-      $(".listheadfour").slideToggle('slow');
-          e.preventDefault();
-      });
-
-
 });
