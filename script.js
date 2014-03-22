e3f17a74cfa0f63f027fe99f28f3b9151d6aa9eb
Daniel Raudschus <draudschus.mmt-b2013@fh-salzburg.ac.at>
2014-03-22 07:33:56 +0100

    repo repariert, nach fehlgeschlagenem merge

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-=======
 
   // pinguin
   $("#teaser").click(function() {
@@ -9,66 +7,41 @@ $(document).ready(function() {
     }, 1000);
   });
 
->>>>>>> mmtproject
 	// scrolling from to the intro location
 	$("#first, #firstScrolle").click(function() {
 		$('html, body').animate({
 			scrollTop : $("#aktuelles").offset().top
-<<<<<<< HEAD
-		}, 2000);
-=======
 		}, 1000);
->>>>>>> mmtproject
 	});
 
 	$("#second, #secondScrolle").click(function() {
 		$('html, body').animate({
 			scrollTop : $("#intro").offset().top
-<<<<<<< HEAD
-		}, 2000);
-=======
 		}, 1000);
->>>>>>> mmtproject
 	});
 
 	$("#third, #thirdScrolle").click(function() {
 		$('html, body').animate({
 			scrollTop : $("#alpin").offset().top
-<<<<<<< HEAD
-		}, 2000);
-=======
 		}, 1000);
->>>>>>> mmtproject
 	});
 
 	$("#forth, #forthScrolle").click(function() {
 		$('html, body').animate({
 			scrollTop : $("#board").offset().top
-<<<<<<< HEAD
-		}, 2000);
-=======
 		}, 1000);
->>>>>>> mmtproject
  	});
 
 	$("#fifth, #fifthScrolle").click(function() {
 		$('html, body').animate({
 			scrollTop : $("#termin").offset().top
-<<<<<<< HEAD
-		}, 2000);
-=======
 		}, 1000);
->>>>>>> mmtproject
 	});
 
 	$("#sixth, #sixthScrolle").click(function() {
     	$('html, body').animate({
         	scrollTop: $("#foerderverein").offset().top
-<<<<<<< HEAD
-    	}, 2000);
-=======
     	}, 1000);
->>>>>>> mmtproject
 	});
 
 	// togglefunction for the subnavigation
@@ -94,24 +67,12 @@ $(document).ready(function() {
           e.preventDefault();
       });
 
-<<<<<<< HEAD
-     var par = $('p');
-      $("#g1").hide();
-      $("#g2").hide();
-=======
       var par = $('g');
       $("#g1").hide();
->>>>>>> mmtproject
 
       $('#g').click(function(e) {
       $("#g1").slideToggle('slow');
           e.preventDefault();
-<<<<<<< HEAD
-                $("#g2").slideToggle('slow');
-          e.preventDefault();
-      });
-
-=======
       });
 
       var par = $('i');
@@ -134,7 +95,6 @@ $(document).ready(function() {
         });
 
 
->>>>>>> mmtproject
       // Slideshow für Bilder in Ski und Snowboard
       $("#skishow1 > div:gt(0)").hide();
         setInterval(function() {
@@ -145,9 +105,6 @@ $(document).ready(function() {
             .end()
             .appendTo('#skishow1');
         },  3000);
-<<<<<<< HEAD
-});
-=======
 
       $("#skishow2 > div:gt(0)").hide();
         setInterval(function() {
@@ -169,4 +126,3 @@ $(document).ready(function() {
             .appendTo('#skishow3');
         },  3000);
 });
->>>>>>> mmtproject
