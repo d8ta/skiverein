2e46cbf8966fa82866913676998ea55ed8b01c8a
Daniel Raudschus <draudschus.mmt-b2013@fh-salzburg.ac.at>
2014-03-22 11:06:10 +0100

    upload.php und .html angepasst

--- upload/upload.php
+++ upload/upload.php
@@ -4,14 +4,14 @@ $uploaddir = dirname( $_SERVER["SCRIPT_FILENAME"] ) . "/pix/";
 $filename = basename($_FILES['bild']['name']);
 $ext = substr($filename, -4);
 
-if( $ext != '.jpg' &&  $ext != '.gif' &&  $ext != '.png' &&  $ext != '.docx' ) {
+if( $ext != '.jpg' &&  $ext != '.gif' &&  $ext != '.png' ) {
    die("es dürfen nur jpg, png oder gif Dateien hochgeladen werden, nicht " . substr($filename, -3) );
 }
 
 $uploadfile = $uploaddir . $filename;
 
 if (move_uploaded_file($_FILES['bild']['tmp_name'], $uploadfile)) {
-  echo "Datei erfolgreich hochgeladen nach <a href='bilder/'>bilder/</a>\n";
+  echo "Datei erfolgreich hochgeladen nach <a href='upload/'>upload/</a>\n";
 } else {
   echo "Problem beim Speichern der Datei.\n";
 }
